de6be691d7d72808563b52dd15cea9af097b29de
Pat Mulhern <patrick.mulhern80@gmail.com>
2020-08-08 06:55:42 -0400

    Add wild card route

--- server.js
+++ server.js
@@ -94,8 +94,9 @@ app.delete("/api/notes/:id",function (req, res) {
 
 });
 
-
-
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
